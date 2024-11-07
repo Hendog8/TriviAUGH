338352dd166d1823abc0fa7bf08a9004e0e2c4d6
Hendog8 <147739615+Hendog8@users.noreply.github.com>
2024-11-07 11:38:05 -0500

    began working on helper methods (methods.js)

---
+++ ta_client/src/methods.js
@@ -0,0 +1,26 @@
+let gamers = [];
+let questions = [];
+
+module.exports = {
+    addGamer:(nickname, id) => {
+        let gamer = {
+            id: id,
+            nickname: nickname,
+            score: 0,
+            selectedAnswers: [],
+        }
+        let newGamers = [...gamers];//man spread syntax is so convenient
+        newGamers.push(gamer);
+        gamers = newGamers;
+        return gamers;
+    },
+    scoringAnswers:(nickname, answers) => {
+        let gamer = gamers.filter(gamer => gamer.nickname === nickname);//what is this brother
+        let newGamers = gamers.filter(gamer => gamer.nickname !== nickname);
+        let scoreAlt = 0;
+        //loop through each correct answer with each answer selected
+        //add one to scoreAlt for each match, subtract one every time there's a selected answer without a single match
+        //mutate score by scoreAlt at the end
+        //actually wait can I just make a "newScore" var and mutate that as we go
+    }
+}
