dff900992b4702c13fe3a39b38190a1f056dadc7
Hendog8 <147739615+Hendog8@users.noreply.github.com>
2024-11-07 11:36:24 -0500

    beginning work with socket

--- ta_client/src/components/gameHub/game.js
+++ ta_client/src/components/gameHub/game.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import io from 'socket.io-client';
+import axios from 'axios';
 //import { connect } from 'react-redux'; what is redux?????
 
 
@@ -9,8 +10,10 @@ class Game extends Component {
         this.state = {
             gamers: [], //array of player objects
                         //includes name, score, and an array of answers for the current question
-            questions: [], //array of question objects 
-                           //each includes question, correct, and incorrect answers
+            questions: [{query:"What?", correct:["who", "how"], incorrect:["where", "why"]},
+                        {query: "which of the following songs place in Mario Judah's top 5 most streamed?", correct:["die very rough", "miss the rage"], incorrect:["all red", "sky"]}], 
+                        //array of question objects 
+                        //each includes question, correct, and incorrect answers
             questionNum: 0, //index of current question
 
             timer: 10, //time in seconds per question
@@ -19,17 +22,20 @@ class Game extends Component {
     }
 
     componentDidMount(){
-        this.importQuestions();
+        //axios may actually be unnecessary
+        this.socket = io('/');
+        this.socket.on('join')
     }
 
     importQuestions(){
-        imported = [{"What?", ["who", "how"], ["where", "why"]}]
+        /*imported = [{query:"What?", correct:["who", "how"], incorrect:["where", "why"]},
+                    {query: "which of the following songs place in Mario Judah's most streamed?", correct:["die very rough", "miss the rage"], incorrect:["all red", "sky"]}];
         this.setState({
-            questions: {"huh"},
-        })
+            questions: imported,
+        })*/
     }
 }
-class Question extends Component {
+/*class Question extends Component {
     constructor(){
         super();
         this.state = {
@@ -38,4 +44,4 @@ class Question extends Component {
             incorrect: [], //same for incorrect, the two should add to 4 answers total
         }
     }
-}
+}*/
