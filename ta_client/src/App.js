c29e921fd7bc42c383d09a7b8c320f82d8cb9cd1
Hendog8 <147739615+Hendog8@users.noreply.github.com>
2024-11-13 14:02:23 -0500

    back to component work

--- ta_client/src/App.js
+++ ta_client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import { useEffect } from "react";
+import React, { Component } from "react";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import io from "socket.io-client";
+import Home from './components/home/Home';
+import { Game, Gamer } from './components/gameHub'
 
 const socket = io.connect("http://localhost:4000");
 
@@ -8,16 +11,26 @@ function App() {
   
   function sendMessage(){
     console.log("HELLLLLLLLLLLLP HELP MEEEEEEEEEEEEEEE");
-    socket.emit("sent_message", { message: "womp womp lil bro" });
+    socket.emit("sent_message", { message: "lmao" });
   }
-  useEffect(() => {
+  /*useEffect(() => { THIS HOOK ISN'T NECESSARY????
     socket.on("received_message", (data) => {
       alert(data.message);
     });
-  }, [socket]);
+  }, [socket]);*/
+  socket.on("received_message", (data) => {
+    alert(data.message);
+  });
   
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' Component = {Home}/>
+          <Route path = '/host' Component = {Game}/>
+          <Route path = '/join' Component = {Gamer}/>
+        </Routes>
+      </BrowserRouter>
       <button onClick={sendMessage}>SOS</button>
     </div>
   );
