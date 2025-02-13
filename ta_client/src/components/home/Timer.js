b6e887514cc851849ab10e4ea2023d8cc84c3e4b
Hendog8 <147739615+Hendog8@users.noreply.github.com>
2025-02-13 11:54:57 -0500

    more timer

--- ta_client/src/components/home/Timer.js
+++ ta_client/src/components/home/Timer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import io from 'socket.io-client';
 
 class Timer extends Component{
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state = {
             length: this.props.time,
                    //maybe this works? unsure about props within constructors
@@ -37,3 +37,5 @@ class Timer extends Component{
         );
     }
 }
+
+export default Timer;
