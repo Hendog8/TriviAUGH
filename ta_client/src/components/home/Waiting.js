3b579910277012915a029429274d686c8e3bf4c6
Hendog8 <147739615+Hendog8@users.noreply.github.com>
2025-02-26 14:09:53 -0500

    converted Waiting to function component

--- ta_client/src/components/home/Waiting.js
+++ ta_client/src/components/home/Waiting.js
@@ -1,42 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Timer from './Timer.js';
 import io from 'socket.io-client';
 import { useNavigate } from 'react-router-dom';
 
-class Waiting extends Component {
-    constructor(){
-        super();
-        this.state = {
-            //idk what this needs in its state ngl
-            //really just a shell for timer to live through
-            //so I can use timer elsewhere without needing 
-            //to render everything within timer's file
+const socket = io.connect('http://localhost:4000');
+
+function Waiting({ tempID }){
+
+    const navigate = useNavigate();
+
+    socket.on('time_up', (data) => {
+        if(data === tempID){
+            console.log("timeto hop in");
+            navigate('/game/game');
+            //not supposed to be here, socket.join????
+        } else {
+            console.log("time for someone else to hop in");
         }
-
-    }
-
-    componentDidMount(){
-        this.socket = io.connect('http://localhost:4000');
-
-        this.socket.on('time_up', (data) => {
-            if(data === this.props.tempID){
-                console.log("time to hop in");
-                useNavigate('/game/game');
-            } else {
-                console.log("time for someone else to hop in");
-            }
-        });
-    }
-
-    render(){
-
-        return(
+    });
+    return(
             <div className="waiting">
                 <p className="w-announcment">Loading, this'll only take a moment</p>
-                <Timer time={10} tempID={this.props.tempID}/>
+                <Timer time={10} tempID={tempID}/>
             </div>
-        )
-    }
+    )
 }
 
 export default Waiting;
